c12f88ab33dff1820c81f3fb92dea7df5c54309f
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-17 17:01:32 +0300

    Project setup.

--- TagView/ContentView.swift
+++ TagView/ContentView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    // View Properties
+    // Sample Tags
+    @State private var tags: [String] = [ "SwiftUI", "Swift", "jos", "Apple", "Xcode", "WWDC", "Android", "React", "Flutter", "App", "Indie", "Developer", "Objc", "C#", "C", "C++", "iPhone", "iPad", "Macbook", "iPadOS", "macOS"
+    ]
+    // Selection
+    @State private var selectedTags: [String] = []
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 0) {
+            ScrollView(.horizontal) {
+                
+            }
         }
-        .padding()
+        .preferredColorScheme(.light)
     }
 }
 
