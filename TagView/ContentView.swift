a4d7acfcfe6680f16a9913a309617c4b50df849f
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-17 17:15:53 +0300

    Building UI.

--- TagView/ContentView.swift
+++ TagView/ContentView.swift
@@ -17,10 +17,68 @@ struct ContentView: View {
     var body: some View {
         VStack(spacing: 0) {
             ScrollView(.horizontal) {
+                HStack(spacing: 12) {
+                    ForEach(selectedTags, id: \.self) { tag in
+                        TagView(tag, .pink, "checkmark")
+                    }
+                }
+                .padding(.horizontal, 15)
+                .frame(height: 35)
+                .padding(.vertical, 15)
+            }
+            .overlay (content: {
+                if selectedTags.isEmpty {
+                    Text("Select More than 3 Tags")
+                        .font(.callout)
+                        .foregroundStyle(.gray)
+                }
+            })
+            .background(.white)
+            
+            ScrollView(.vertical) {
                 
             }
+            .background(.black.opacity (0.05))
+            
+            ZStack {
+                Button(action: {}, label: {
+                    Text("Continue")
+                        .fontWeight(.semibold)
+                        .padding(.vertical, 15)
+                        .frame(maxWidth: .infinity)
+                        .foregroundStyle(.white)
+                        .background {
+                            RoundedRectangle(cornerRadius: 12)
+                                .fill(.pink.gradient)
+                        }
+                })
+                // Disabling until 3 or more tags selected
+                .disabled(selectedTags.count < 3)
+                .opacity(selectedTags .count < 3 ? 0.5 : 1)
+                .padding()
+            }
+            .background(.white)
         }
         .preferredColorScheme(.light)
+    }
+    
+    // Tag View
+    @ViewBuilder
+    func TagView(_ tag: String, _ color: Color, _ icon: String) -> some View {
+        HStack(spacing: 10) {
+            Text(tag)
+                .font(.callout)
+                .fontWeight(.semibold)
+            
+            Image(systemName: icon)
+        }
+        .frame(height: 35)
+        .foregroundStyle(.white)
+        .padding(.horizontal, 15)
+        .background {
+            Capsule()
+                .fill(color.gradient)
+        }
     }
 }
 
