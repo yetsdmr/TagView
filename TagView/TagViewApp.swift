3a338538fa466323d2d05d22314e8b5e96dc45c6
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-17 16:51:34 +0300

    Initial Commit

---
+++ TagView/TagViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TagViewApp.swift
+//  TagView
+//
+//  Created by Yunus Emre Taşdemir on 17.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct TagViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
